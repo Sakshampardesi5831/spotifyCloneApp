3fab379d6b2ed3202ae1cab31b46a80a149115d0
Sakshampardesi5831 <sakshampardesi5831@gmail.com>
2023-05-05 00:39:10 +0530

    login page created

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+let userModel=require("./users");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
+router.get("/login",function(req,res){
+    res.render("login");
+})
 module.exports = router;
