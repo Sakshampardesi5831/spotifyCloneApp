f2f0a8211a25eb917e9837fd8607499249115825
Sakshampardesi5831 <sakshampardesi5831@gmail.com>
2023-06-02 10:01:18 +0530

    some changes

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,12 @@
 
 const mongoose=require("mongoose");
 var plm=require("passport-local-mongoose");
-mongoose.connect("mongodb://localhost/spotifyDb").then(function(){
-  console.log("Connected to db");
-})
-// mongoose.connect(`mongodb+srv://sakshampardesi5831:${process.env.PASSWORD}@cluster0.svpwpzm.mongodb.net/spotifyDb?retryWrites=true&w=majority`).then(function(){
+// mongoose.connect("mongodb://localhost/spotifyDb").then(function(){
 //   console.log("Connected to db");
 // })
+mongoose.connect(`mongodb+srv://sakshampardesi5831:${process.env.PASSWORD}@cluster0.svpwpzm.mongodb.net/spotifyDb?retryWrites=true&w=majority`).then(function(){
+  console.log("Connected to db");
+})
 var userSchema= mongoose.Schema({
   username:String,
   name:String,
@@ -44,6 +44,6 @@ var userSchema= mongoose.Schema({
     type:mongoose.Schema.Types.ObjectId,
     ref:"album"
    }]
-})
+},{timestamps:true})
 userSchema.plugin(plm);
 module.exports=mongoose.model("user",userSchema);
